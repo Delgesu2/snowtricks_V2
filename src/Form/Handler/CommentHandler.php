00ea05434eb2918991a1d2a68c074431d0f417ab
delgesu2 <xavierus70@hotmail.com>
2018-12-21 21:34:11 +0100

    Current work on comment

--- src/Form/Handler/CommentHandler.php
+++ src/Form/Handler/CommentHandler.php
@@ -9,6 +9,7 @@
 namespace App\Form\Handler;
 
 use App\Entity\Comment;
+use App\Entity\Trick;
 use App\Form\Type\Trick\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\UnitOfWork;
@@ -21,17 +22,25 @@ class CommentHandler extends AbstractHandler
      */
     private $entityManager;
 
+    /**
+     * @var Trick
+     */
+    private $trick;
+
 
     /**
      * CommentHandler constructor.
      *
      * @param EntityManagerInterface $entityManager
+     * @param Trick $trick
      */
     public function __construct(
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Trick                  $trick
     )
     {
         $this->entityManager = $entityManager;
+        $this->trick         = $trick;
     }
 
 
@@ -40,10 +49,9 @@ class CommentHandler extends AbstractHandler
      */
     public function onSuccess(): void
     {
-        if($this->entityManager->getUnitOfWork()->getEntityState($this->data) === UnitOfWork::STATE_NEW) {
 
-         dump($this->form->getData());
-         die;
+
+        if($this->entityManager->getUnitOfWork()->getEntityState($this->data) === UnitOfWork::STATE_NEW) {
 
             $this->entityManager->persist($this->data);
         }
